84c9ddeab8dfd295e91b06d7734d1664856bb93e
aki-anz <aki.anz.algo.io@gmail.com>
2020-09-16 19:57:04 +0800

    Updates

---
+++ precache-manifest.dfe2489f2f7a2bd0533bd3ee4dd654d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffb1c8a08c0ed461a18338932ad5259",
+    "url": "/jupiter-business-app/index.html"
+  },
+  {
+    "revision": "679e5f57c47bde5cb3f2",
+    "url": "/jupiter-business-app/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "f5eb29057ae88c618b51",
+    "url": "/jupiter-business-app/static/css/main.45be5b85.chunk.css"
+  },
+  {
+    "revision": "679e5f57c47bde5cb3f2",
+    "url": "/jupiter-business-app/static/js/2.405cca12.chunk.js"
+  },
+  {
+    "revision": "a52153bcc847b73a84a3dde72b2b9c2e",
+    "url": "/jupiter-business-app/static/js/2.405cca12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5eb29057ae88c618b51",
+    "url": "/jupiter-business-app/static/js/main.2be5daeb.chunk.js"
+  },
+  {
+    "revision": "fc47023afd9542bbbef0",
+    "url": "/jupiter-business-app/static/js/runtime-main.4badbbce.js"
+  },
+  {
+    "revision": "7fa0e125ac8f7c498c17978775aa47e6",
+    "url": "/jupiter-business-app/static/media/stop.7fa0e125.png"
+  }
+]);
